73f8178f432ed949dc98e8a51d729c883baa7942
santron1 <sanchayk396@gmail.com>
2024-09-04 00:38:55 +0530

    all setup configured

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error{
         statusCode,
         message= "Kuch toa ho gaya hai",
         errors = [],
-        statck =""
+        stack = ""
     ){
         super(message)
         this.statusCode = statusCode
@@ -11,10 +11,11 @@ class ApiError extends Error{
         this.message= message
         this.success = false;
         this.errors = errors
-        if(statck){
-            this.statck = statck
+        if(stack){
+            this.stack = stack
         }else{
-            Error.captureStackTrace(this,this.constructor)
+            //Error.captureStackTrace(this,this.constructor)
+            Error.captureStackTrace(this, this.constructor)
         }
     }
 }
